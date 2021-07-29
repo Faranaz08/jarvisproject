f73640272fe5b2757b7cdeb67be13c10fc627eb2
Faranaz08 <83759275+Faranaz08@users.noreply.github.com>
2021-07-29 10:50:45 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+    # #logic for executing tasks based on query
+    #     if 'wikipedia' in query:
+    #         speak('Searching Wikipedia...')
+    #         query = query.replace("wikipedia","")
+    #         results = wikipedia.summary(query, sentences=2)
+    #         speak("According to wikipedia")
+    #         speak(results)
+
+    #     elif 'open youtube' in query:
+    #         webbrowser.open("youtube.com")
